4bec5d724ff6f939534177f9b55356cf25a5dde0
MOAZ AHMAD <moazahmad.se@gmail.com>
2025-06-27 19:13:23 +0500

    Fix: Rename app.jsx to App.jsx to match import path for Vercel

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,4 +1,5 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+import App from "../pages/App";
 import Home from "../pages/Home";
 import Group from "../pages/Group";
 import Account from "../pages/Account";
